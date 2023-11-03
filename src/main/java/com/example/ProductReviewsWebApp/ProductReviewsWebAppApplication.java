3e31445edba2761009f71ad98c9ef1e6c824d984
Nathan MacDiarmid <nathanmacdiarmid@gmail.com>
2023-11-03 16:15:50 -0400

    added skeleton for project

---
+++ src/main/java/com/example/ProductReviewsWebApp/ProductReviewsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductReviewsWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReviewsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductReviewsWebAppApplication.class, args);
+	}
+
+}
