ce9ccca5e2cd5cf2fe1cad9a3cfbd3304fb5747c
trong0dn <55768917+trong0dn@users.noreply.github.com>
2023-11-14 21:29:22 -0500

    Added SecurityConfigTest

--- src/main/java/com/example/ProductReviewsWebApp/configuration/SecurityConfig.java
+++ src/main/java/com/example/ProductReviewsWebApp/configuration/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(antMatcher("/")).permitAll()
                         .requestMatchers(antMatcher("/api/**")).permitAll()
---
+++ src/test/java/com/example/ProductReviewsWebApp/SecurityConfigTest.java
@@ -0,0 +1,78 @@
+package com.example.ProductReviewsWebApp;
+
+import com.example.ProductReviewsWebApp.configuration.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class SecurityConfigTest {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private SecurityConfig securityConfig;
+
+    @Test
+    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() throws Exception {
+        ResponseEntity<String> result = template.withBasicAuth("admin", "password")
+                .getForEntity("/private/hello", String.class);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    public void givenBasicUser_whenGetFooSalute_then3xxRedirection() throws Exception
+    {
+        mockMvc.perform(MockMvcRequestBuilders.get("/foo/salute")
+                        .accept(MediaType.ALL))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void givenManagerUser_whenGetFooSalute_thenOk() throws Exception
+    {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api")
+                        .accept(MediaType.ALL))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void test_UserPassword() {
+        String password = securityConfig.userDetailsService().loadUserByUsername("user").getPassword();
+        assertEquals(password, "{noop}password");
+    }
+
+    @Test
+    public void test_User_isEnabled() {
+        assertTrue(securityConfig.userDetailsService().loadUserByUsername("user").isEnabled());
+    }
+    @Test
+    public void test_AdminPassword() {
+        String password = securityConfig.userDetailsService().loadUserByUsername("admin").getPassword();
+        assertEquals(password, "{noop}password");
+    }
+    @Test
+    public void test_Admin_isEnabled() {
+        assertTrue(securityConfig.userDetailsService().loadUserByUsername("admin").isEnabled());
+    }
+
+    @Test
+    public void test_NotLoggedInUser_isNotAuthenticated() {
+        assertFalse(securityConfig.isAuthenticated());
+    }
+}
