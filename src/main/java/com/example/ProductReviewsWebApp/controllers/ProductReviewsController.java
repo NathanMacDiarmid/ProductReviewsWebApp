3997b3cbfb12f8cac4476e2026cc789c135a690d
koookee <55768904+koookee@users.noreply.github.com>
2023-11-23 17:24:26 -0500

    Review form is no longer displayed if the user has submitted a review for that product

--- src/main/java/com/example/ProductReviewsWebApp/controllers/ProductReviewsController.java
+++ src/main/java/com/example/ProductReviewsWebApp/controllers/ProductReviewsController.java
@@ -110,8 +110,11 @@ public class ProductReviewsController {
             if(Objects.equals(review.getProduct().getId(), product.getId())) reviewsForProduct.add(review);
         }
 
+        Client client = clientRepository.findByUsername("TestClient"); // TODO Replace with logged in client
+
         model.addAttribute("reviews", reviewsForProduct);
         model.addAttribute("product", product);
+        model.addAttribute("hasReview", client.hasReviewForProduct(id));
         return "product-page";
     }
 
