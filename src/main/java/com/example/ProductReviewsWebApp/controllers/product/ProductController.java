c89043a430bd023c495529916eef9db115a00477
Jeremy Trendoff <jeremytrendoff@rogers.com>
2023-11-27 11:35:24 -0500

    Refactored Controllers

---
+++ src/main/java/com/example/ProductReviewsWebApp/controllers/product/ProductController.java
@@ -0,0 +1,92 @@
+package com.example.ProductReviewsWebApp.controllers.product;
+
+import com.example.ProductReviewsWebApp.models.Product;
+import com.example.ProductReviewsWebApp.models.Review;
+import com.example.ProductReviewsWebApp.repositories.ClientRepository;
+import com.example.ProductReviewsWebApp.repositories.ProductRepository;
+import com.example.ProductReviewsWebApp.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    private Product getProduct(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity not found");
+        }
+        return product.get();
+    }
+
+    @GetMapping(value="/product")
+    public String getProducts(Model model) {
+        List<Product> productList = productRepository.findAll();
+        model.addAttribute("ProductList", productList);
+        return "product";
+    }
+
+    @GetMapping(value="/product/{id}", produces="application/json")
+    public String getProductById(@CookieValue(value = "activeClientID") String activeClientId, @PathVariable("id") Long id, Model model) {
+        Product product = productRepository.findById(id).orElse(null);
+        if (product == null) throw new NullPointerException("Was not able to find product with the passed ID");
+        ArrayList<Review> reviewsForProduct = new ArrayList<>();
+
+        for(Review review : reviewRepository.findAll()) {
+            if(Objects.equals(review.getProduct().getId(), product.getId())) reviewsForProduct.add(review);
+        }
+
+        // Client client = clientRepository.findByUsername("TestClient"); // TODO Replace with logged in client
+
+        model.addAttribute("reviews", reviewsForProduct);
+        model.addAttribute("product", product);
+        // model.addAttribute("hasReview", client.hasReviewForProduct(id));
+        return "product-page";
+    }
+
+    @PostMapping(value="/product", consumes="application/json", produces="application/json")
+    public Product createProduct(@RequestBody Product product) {
+        productRepository.save(product);
+        return product;
+    }
+
+    @PutMapping(value="/product/{id}", consumes="application/json", produces="application/json")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product newProduct) {
+        Product product = getProduct(id);
+        if (newProduct.getName() != null) {
+            product.setName(newProduct.getName());
+        }
+        if (newProduct.getUrl() != null) {
+            product.setUrl(newProduct.getUrl());
+        }
+        if (newProduct.getCategory() != null) {
+            product.setCategory(newProduct.getCategory());
+        }
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping(value="/product/{id}")
+    public Product deleteProduct(@PathVariable Long id) {
+        Product product = getProduct(id);
+        productRepository.deleteById(id);
+        return product;
+    }
+}
---
+++ src/main/java/com/example/ProductReviewsWebApp/controllers/review/ReviewController.java
@@ -0,0 +1,149 @@
+package com.example.ProductReviewsWebApp.controllers.review;
+
+import com.example.ProductReviewsWebApp.models.Client;
+import com.example.ProductReviewsWebApp.models.Product;
+import com.example.ProductReviewsWebApp.models.Review;
+import com.example.ProductReviewsWebApp.repositories.ClientRepository;
+import com.example.ProductReviewsWebApp.repositories.ProductRepository;
+import com.example.ProductReviewsWebApp.repositories.ReviewRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Controller
+public class ReviewController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    private Product getProduct(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity not found");
+        }
+        return product.get();
+    }
+
+    private Client getClient(Long id) {
+        Optional<Client> client = clientRepository.findById(id);
+        if (client.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity not found");
+        }
+        return client.get();
+    }
+
+    private String getAuthorOfReview(Review review) {
+        List<Client> clientList = clientRepository.findAll();
+
+        for (Client c : clientList) {
+            if (c.hasReviewByReviewId(review.getId())) {
+                return c.getUsername();
+            }
+        }
+
+        return "";
+    }
+
+    private Review getReview(Long id) {
+        Optional<Review> review = reviewRepository.findById(id);
+        if (review.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity not found");
+        }
+        return review.get();
+    }
+
+    @GetMapping(value="/review", produces="application/json")
+    public String getReviews(Model model, HttpServletResponse response) {
+        List<Review> reviewList = reviewRepository.findAll();
+        model.addAttribute("ReviewList", reviewList);
+
+        for (Review review : reviewList) {
+            String authorCookieName = "authorOfReview" + Long.toString(review.getId());
+            Cookie authorCookie = new Cookie(authorCookieName, URLEncoder.encode(this.getAuthorOfReview(review), StandardCharsets.UTF_8));
+            response.addCookie(authorCookie);
+        }
+        return "review";
+    }
+
+    @GetMapping(value="/review/{id}", produces="application/json")
+    public String getReviewById(@PathVariable("id") Long id, Model model, HttpServletResponse response) {
+        Review review = getReview(id);
+
+
+        Client authorOfReview = clientRepository.findByUsername(this.getAuthorOfReview(review));
+
+        model.addAttribute("author", authorOfReview.getUsername());
+
+        String authorCookieName = "authorOfReview" + id.toString();
+        Cookie authorCookie = new Cookie(authorCookieName, authorOfReview.getId().toString());
+        response.addCookie(authorCookie);
+
+        model.addAttribute("review", review);
+        return "review-page";
+    }
+
+    @PostMapping(value="/review", consumes="application/json", produces="application/json")
+    public Review createReview(@RequestBody Review review) {
+        reviewRepository.save(review);
+        return review;
+    }
+
+    @PostMapping(value="/submitReview")
+    public String addReview(@CookieValue(value = "activeClientID") String activeClientId, @RequestParam(value = "reviewComment") String reviewComment, @RequestParam(value = "productId") long productId, @RequestParam(value = "rating") int reviewRating, Model model) {
+        Product product = getProduct(productId);
+        Review review = new Review(reviewRating, reviewComment, product);
+        productRepository.save(product);
+        Client client = getClient(Long.parseLong(activeClientId));
+
+        client.addReviewForProduct(productId, review);
+        clientRepository.save(client);
+
+        ArrayList<Review> reviewsForProduct = new ArrayList<>();
+        for(Review fetchedReview : reviewRepository.findAll()) {
+            if(Objects.equals(fetchedReview.getProduct().getId(), product.getId())) reviewsForProduct.add(fetchedReview);
+        }
+
+        model.addAttribute("reviews", reviewsForProduct);
+        model.addAttribute("product", product);
+        model.addAttribute("hasReview", client.hasReviewForProduct(productId));
+        return "product-page";
+    }
+
+    @PutMapping(value="/review/{id}", consumes="application/json", produces="application/json")
+    public Review updateReview(@PathVariable Long id, @RequestBody Review newReview) {
+        Review review = getReview(id);
+        review.setRating(newReview.getRating());
+        if (newReview.getComment() != null) {
+            newReview.setComment(newReview.getComment());
+        }
+        return reviewRepository.save(review);
+    }
+
+    // This method is only for the sake of testing
+    // Do not use this method to delete a review from a product
+    // or else an error will be thrown
+    @DeleteMapping(value="/review/{id}")
+    public Review deleteReview(@PathVariable Long id) {
+        Review review = getReview(id);
+        reviewRepository.deleteById(id);
+        return review;
+    }
+}
