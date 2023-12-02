3900bc3ac3087edd36eec43e93062ab6b02f4893
Evan Smedley <evan.smedley22@gmail.com>
2023-12-02 12:02:26 -0500

    Add mockito dependency and fix product test

--- src/test/java/com/example/ProductReviewsWebApp/ProductRestControllerTest.java
+++ src/test/java/com/example/ProductReviewsWebApp/ProductRestControllerTest.java
@@ -3,25 +3,30 @@ package com.example.ProductReviewsWebApp;
 import com.example.ProductReviewsWebApp.models.Category;
 import com.example.ProductReviewsWebApp.models.Product;
 import com.example.ProductReviewsWebApp.repositories.ProductRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ProductTest {
+@ExtendWith(MockitoExtension.class)
+public class ProductRestControllerTest {
 
     @Value(value="${local.server.port}")
     private int port;
@@ -29,31 +34,43 @@ public class ProductTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Mock
-    private ProductRepository productRepository;
+    @MockBean
+    private static ProductRepository mockProductRepository;
 
-    private Product pizza;
+    private List<Product> products;
 
-    private Product shawarma;
+    @PostConstruct
+    private void setup() {
+        this.products = generateProducts();
 
-    @BeforeEach
-    public void setup() {
-        // Add two test products to the product list
-        pizza = new Product("www.pizza.com", "pizza", Category.FOOD);
-        shawarma = new Product("www.shawarma.com", "shawarma", Category.FOOD);
-        productRepository.save(pizza);
-        productRepository.save(shawarma);
+        when(mockProductRepository.findAll()).thenReturn(products);
+
+        for (int i = 0; i < products.size(); i++) {
+            when(mockProductRepository.findById((long) i)).thenReturn(Optional.ofNullable(products.get(i)));
+            when(mockProductRepository.save(products.get(i))).thenReturn(products.get(i));
+        }
+
+
     }
 
-    @AfterEach
-    public void tearDown() {
-        // Clear all persisted products
-        productRepository.delete(pizza);
-        productRepository.delete(shawarma);
+    private List<Product> generateProducts() {
+        List<Product> productList = List.of(
+                new Product("www.pizza.com", "pizza", Category.FOOD),
+                new Product("www.spider-plant.com", "spider", Category.PLANT),
+                new Product("www.harry-potter.com", "Harry Potter and the Philosopher's Stone", Category.BOOK)
+        );
+
+        long id = 0L;
+        for (Product product : productList) {
+            product.setId(id++);
+            System.out.println(product.getId());
+        }
+
+        return productList;
     }
 
     @Test
-    public void getProductsTest() {
+    public void testGetProducts() {
 
         // GIVEN
         String resourceUrl = "http://localhost:" + port + "/api/product";
@@ -68,11 +85,13 @@ public class ProductTest {
         assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
     }
 
-    @Test
-    public void getProductTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    public void getProductTest(int index) {
         // GIVEN
-        Product testProduct = productRepository.findByName("pizza");
+        Product testProduct = products.get(index);
         String resourceUrl = "http://localhost:" + port + "/api/product/" + testProduct.getId();
+        System.out.println(resourceUrl);
 
         // WHEN
         ResponseEntity<Product> response = restTemplate.getForEntity(resourceUrl, Product.class);
@@ -80,9 +99,9 @@ public class ProductTest {
         // THEN
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
-        assertEquals("pizza", Objects.requireNonNull(response.getBody()).getName());
-        assertEquals("www.pizza.com", response.getBody().getUrl());
-        assertEquals(Category.FOOD, response.getBody().getCategory());
+        assertEquals(testProduct.getName(), Objects.requireNonNull(response.getBody()).getName());
+        assertEquals(testProduct.getUrl(), response.getBody().getUrl());
+        assertEquals(testProduct.getCategory(), response.getBody().getCategory());
     }
 
     @Test
@@ -101,33 +120,29 @@ public class ProductTest {
         assertEquals("spaghetti", Objects.requireNonNull(response.getBody()).getName());
         assertEquals("www.spaghetti.com", response.getBody().getUrl());
         assertEquals(Category.FOOD, response.getBody().getCategory());
-
-        assertNotNull(productRepository.findByName("spaghetti"));
-
-        productRepository.delete(productRepository.findByName("spaghetti"));
     }
 
-    @Test
-    public void deleteProductTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    public void deleteProductTest(int index) {
 
         // GIVEN
-        HttpEntity<Product> request = new HttpEntity<>(new Product("www.spaghetti.com", "spaghetti", Category.FOOD));
+        HttpEntity<Product> request = new HttpEntity<>(products.get(index));
         String resourceUrl = "http://localhost:" + port + "/api/product";
 
-        restTemplate.postForEntity(resourceUrl, request, Product.class);
+        ResponseEntity<Product> response = restTemplate.postForEntity(resourceUrl, request, Product.class);
 
-        productRepository.delete(productRepository.findByName("spaghetti"));
-        assertNull(productRepository.findByName("spaghetti"));
+        assertEquals(products.get(index).getName(), Objects.requireNonNull(response.getBody()).getName());
     }
 
     @Test
     public void updateProductTest() {
 
         // GIVEN
+        int index = 0;
         HttpEntity<Product> updatedEntity =
-                new HttpEntity<>(new Product("www.pizza.com", "piizzzzzaaaa", Category.FOOD));
-        Long id = productRepository.findByName("pizza").getId();
-        String resourceUrl = "http://localhost:" + port + "/api/product/" + id;
+                new HttpEntity<>(products.get(index));
+        String resourceUrl = "http://localhost:" + port + "/api/product/" + products.get(index).getId();
 
         // WHEN
         ResponseEntity<Product> response = restTemplate.exchange(resourceUrl, HttpMethod.PUT, updatedEntity, Product.class);
@@ -135,12 +150,8 @@ public class ProductTest {
         // THEN
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
-        assertEquals("piizzzzzaaaa", Objects.requireNonNull(response.getBody()).getName());
-        assertEquals("www.pizza.com", response.getBody().getUrl());
-        assertEquals(Category.FOOD, response.getBody().getCategory());
-
-        productRepository.findById(id).ifPresent(
-                product -> assertEquals("piizzzzzaaaa", product.getName())
-        );
+        assertEquals(products.get(index).getName(), Objects.requireNonNull(response.getBody()).getName());
+        assertEquals(products.get(index).getUrl(), response.getBody().getUrl());
+        assertEquals(products.get(index).getCategory(), response.getBody().getCategory());
     }
 }
